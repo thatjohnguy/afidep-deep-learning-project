d27b7d8ce2b89cd308a70715a5ec35b3d80feaa1
thatjohnguy <johnomoluabimail@gmail.com>
2022-12-13 22:32:47 +0100

    added path engine

---
+++ dataset_dir.py
@@ -0,0 +1,13 @@
+'''All dataset paths'''
+def csv_dir():
+    location="C:\\Users\\Ayo\\Documents\\AKENA_WORK_FILES\\Documents\\datasets\\csv\\"
+    return location
+def excel_dir():
+    location="C:\\Users\\Ayo\\Documents\\AKENA_WORK_FILES\\Documents\\datasets\\excel\\"
+    return location
+def stata_dir():
+    location="C:\\Users\\Ayo\\Documents\\AKENA_WORK_FILES\\Documents\\datasets\\dta\\"
+    return location
+def gis_dir():
+    location="C:\\Users\\Ayo\\Documents\\AKENA_WORK_FILES\\Documents\\datasets\\gis\\"
+    return location
